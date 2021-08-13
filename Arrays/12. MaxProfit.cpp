aed5215605525d71c4e63b34bf9ae3f381c4c2c9
jatin <jatin@captainfresh.in>
2021-08-14 02:26:00 +0530

    MAX PROFIT

---
+++ Arrays/12. MaxProfit.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+
+        int ans = 0;
+
+        for (int i = 1; i < prices.size(); i++)
+        {
+
+            if (prices[i] > prices[i - 1])
+            {
+                ans += prices[i] - prices[i - 1];
+            }
+        }
+        return ans;
+    }
+};
